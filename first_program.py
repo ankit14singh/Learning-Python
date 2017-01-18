be2a95feb7a5b16f8b61ad439946a737fd946d4d
ankit14singh <ankittco16@gmail.com>
2017-01-18 21:54:32 +0530

    Add files via upload

---
+++ first_program.py
@@ -0,0 +1,11 @@
+#This program asks my name as input and prints it.
+print('Hello World!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you ,' +myName)
+print('The length of your name is')
+print(len(myName))
+print('What is your age?')
+myAge = input()
+print('My age is ' +str(int(myAge) +1))
+
